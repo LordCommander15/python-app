f8a5ab02b81cf60e3e18d6d5ebb96f2aa19eced0
LordCommander15 <lordcommander15@github.com>
2026-04-29 11:10:43 +0200

    Change app.py and runer for self hosted in kind

--- source/app.py
+++ source/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime('%I:%M:%S%p on %B %d, %Y'),
         'hostname': socket.gethostname()
-        'message': 'You are on the right track'
+        'message': 'You are on the right track!'
     })
 
 @app.route('/api/v1/healthz')
